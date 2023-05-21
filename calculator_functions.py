8e39f225f9777af17f9866f29826561c6bd80ced
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-05-21 22:04:55 +0800

    initial pseudocodes

--- calculator_functions.py
+++ calculator_functions.py
@@ -2,3 +2,6 @@
 # BSCpE 1-5
 # Object-Oriented Programming | Assignment 7
 # Remodified Object-Oriented Simple Calculator
+
+# define a Calculator Functions class with its properties and methods
+# execute Calculator Functions class methods
--- calculator_ui.py
+++ calculator_ui.py
@@ -2,3 +2,10 @@
 # BSCpE 1-5
 # Object-Oriented Programming | Assignment 7
 # Remodified Object-Oriented Simple Calculator
+
+# Pseudocode
+
+# define a Calculator UI class with its properties and methods
+# enable Calculator UI class constructor/s
+# execute Calculator UI class methods
+# create GUI elements and configure their properties
